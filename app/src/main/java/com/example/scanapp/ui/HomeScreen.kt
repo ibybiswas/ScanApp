401d37792419aac4009399821f32569a8bbb6b5f
ibybiswas <ibnebony100@gmail.com>
2026-07-08 17:40:30 +0530

    bug fix

--- app/src/main/java/com/example/scanapp/ui/HomeScreen.kt
+++ app/src/main/java/com/example/scanapp/ui/HomeScreen.kt
@@ -45,7 +45,6 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.input.pointer.pointerInput
-import androidx.compose.ui.input.pointer.awaitFirstDown
 import androidx.compose.ui.input.pointer.positionChange
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.layout.onGloballyPositioned
